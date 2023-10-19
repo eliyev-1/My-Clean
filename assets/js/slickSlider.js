c01b1ab400638c5092e23ab99b17f5492d331378
Ali <designman699@gmil.com>
2023-10-19 16:36:05 +0400

    Home ve haqqimizda sehifesi hazirdi

---
+++ assets/js/slickSlider.js
@@ -0,0 +1,132 @@
+$('.services__slider').slick({
+      dots: false,
+      prevArrow: '<div class="custom-arrow prev-arrow"><i class="fa-solid fa-arrow-right"></i></div>',
+    nextArrow: '<div class="custom-arrow next-arrow"><i class="fa-solid fa-arrow-left"></i></div>',
+   
+   autoplay: false,
+  
+   autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 750,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 0,
+            prevArrow:'',
+            nextArrow:''
+          }
+        },
+       
+    
+      ]
+    });
+    $('.products__slider').slick({
+      dots: true,
+      arrows: false,
+   autoplay: false,
+  
+   autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 750,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 0,
+          
+          }
+        },
+       
+    
+      ]
+    });
+
+    $('.cars__slider').slick({
+      dots: true,
+      arrows: false,
+   autoplay: false,
+  
+   autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 750,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 0,
+          
+          }
+        },
+       
+    
+      ]
+    });
+
+    $('.blog__slider').slick({
+      dots: false,
+      prevArrow: '<div class="custom-arrow prev-arrow"><i class="fa-solid fa-arrow-right"></i></div>',
+    nextArrow: '<div class="custom-arrow next-arrow"><i class="fa-solid fa-arrow-left"></i></div>',
+   
+   autoplay: false,
+  
+   autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 750,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 0,
+            prevArrow:'',
+            nextArrow:''
+          }
+        },
+       
+    
+      ]
+    });
