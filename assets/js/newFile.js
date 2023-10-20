fbd642fc132eba9f684501c8b69b563827c7191b
Ali <designman699@gmil.com>
2023-10-20 17:13:50 +0400

    about, services servicesDetail products sehifesi hazirdir

---
+++ assets/js/newFile.js
@@ -0,0 +1,32 @@
+$(".services__slider").slick({
+  dots: false,
+  prevArrow:
+    '<div class="custom-arrow prev-arrow"><i class="fa-solid fa-arrow-right"></i></div>',
+  nextArrow:
+    '<div class="custom-arrow next-arrow"><i class="fa-solid fa-arrow-left"></i></div>',
+  arrows: true,
+  infinite: true,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 0,
+      },
+    },
+  ],
+});
--- assets/js/slickSlider.js
+++ assets/js/slickSlider.js
@@ -1,37 +1,35 @@
 $('.services__slider').slick({
-      dots: false,
-      prevArrow: '<div class="custom-arrow prev-arrow"><i class="fa-solid fa-arrow-right"></i></div>',
-    nextArrow: '<div class="custom-arrow next-arrow"><i class="fa-solid fa-arrow-left"></i></div>',
-   
-   autoplay: false,
-  
-   autoplaySpeed: 2000,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 750,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 0,
-            prevArrow:'',
-            nextArrow:''
-          }
-        },
-       
-    
-      ]
-    });
+  dots: false,
+  prevArrow: '<div class="custom-arrow prev-arrow"><i class="fa-solid fa-arrow-right"></i></div>',
+  nextArrow: '<div class="custom-arrow next-arrow"><i class="fa-solid fa-arrow-left"></i></div>',
+  arrows: true,
+  infinite: true,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 0,
+      
+      }
+    }
+  ]
+});
+
     $('.products__slider').slick({
       dots: true,
       arrows: false,
@@ -102,6 +100,7 @@ $('.services__slider').slick({
     nextArrow: '<div class="custom-arrow next-arrow"><i class="fa-solid fa-arrow-left"></i></div>',
    
    autoplay: false,
+   arrows: true,
   
    autoplaySpeed: 2000,
       speed: 300,
@@ -122,8 +121,7 @@ $('.services__slider').slick({
           settings: {
             slidesToShow: 1,
             slidesToScroll: 0,
-            prevArrow:'',
-            nextArrow:''
+           
           }
         },
        
