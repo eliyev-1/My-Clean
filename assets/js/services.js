1786f5e3dde39746402c4de11795d63695c662f1
Ali <designman699@gmil.com>
2023-11-03 15:27:34 +0400

    save

--- assets/js/services.js
+++ assets/js/services.js
@@ -106,9 +106,13 @@ return response.json()
     nextArrow: '',
     dots: true,
     infinite: true,
+   
+  
+  
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 4,
+   
     responsive: [
       {
         breakpoint: 1024,
